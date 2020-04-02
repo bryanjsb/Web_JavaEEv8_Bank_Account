6e68c1900b9915671402dd4d554132b08f56dc7c
Bryan Sánchez B <bryan.jsb.1801@gmail.com>
2020-04-02 01:31:55 -0600

    eliminando servelets

--- Proyecto (diseño)/carga inicial.sql
+++ Proyecto (diseño)/carga inicial.sql
@@ -8,3 +8,10 @@ INSERT INTO `eif209_2001_p01`.`moneda`
 		('EUR', 'Euro', '€', '700.0', '720.0')
 	;
 SELECT * FROM  `eif209_2001_p01`.`moneda`;
+
+
+INSERT INTO `eif209_2001_p01`.`usuario` (`id_usuario`, `clave_acceso`, `clave_vencida`, `rol`) VALUES ('1', '1', '1', '1');
+INSERT INTO `eif209_2001_p01`.`usuario` (`id_usuario`, `clave_acceso`, `clave_vencida`, `rol`) VALUES ('0', '0', '0', '0');
+INSERT INTO `eif209_2001_p01`.`usuario` (`id_usuario`, `clave_acceso`, `clave_vencida`, `rol`) VALUES ('3', '3', '3', '1');
+
+SELECT * FROM eif209_2001_p01.usuario;
