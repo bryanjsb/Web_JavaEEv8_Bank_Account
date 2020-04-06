1be3c0360b33668ee2ec53fcdd9e6776a856074f
Tannya <tannyahibbert2103@gmail.com>
2020-04-06 12:54:59 -0500

    Update DaoCuenta

--- cuentaBancaria/src/java/modelo/cuenta/DaoCuenta.java
+++ cuentaBancaria/src/java/modelo/cuenta/DaoCuenta.java
@@ -5,7 +5,6 @@
  */
 package modelo.cuenta;
 
-import modelo.cliente.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -19,20 +18,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import modelo.datos.BaseDatosBanco;
 
-
-
-/**
- *
- * @author Bryan
- */
 public class DaoCuenta {
-    
-    public boolean verificarCliente(String idCliente) {
+
+    public boolean verificarCliente(String numCuenta) {
         boolean encontrado = false;
         try (Connection cnx = obtenerConexion();
                 PreparedStatement stm = cnx.prepareStatement(CRUD_Cuenta.VERIFICAR.obtenerComando())) {
             stm.clearParameters();
-            stm.setString(1, idCliente);
+            stm.setString(1, numCuenta);
             ResultSet rs = stm.executeQuery();
             encontrado = rs.next();
         } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | IOException | SQLException ex) {
@@ -43,20 +36,25 @@ public class DaoCuenta {
         return encontrado;
     }
 
-    public Optional<Cliente> obtenerCliente(String idCliente) {
-        Optional<Cliente> r = Optional.empty();
+    public Optional<cuenta> obtenerCliente(String numCuenta) {
+        Optional<cuenta> r = Optional.empty();
         try (Connection cnx = obtenerConexion();
                 PreparedStatement stm = cnx.prepareStatement(CRUD_Cuenta.CONSULTAR.obtenerComando());) {
             stm.clearParameters();
-            stm.setString(1, idCliente);
+            stm.setString(1, numCuenta);
             try (ResultSet rs = stm.executeQuery()) {
                 if (rs.next()) {
-                    r = Optional.of(new Cliente(
-                           rs.getString("id_cliente"),
-                        rs.getString("usuario_id_usuario"),
-                        rs.getString("apellidos"),
-                        rs.getString("nombre"),
-                        rs.getString("telefono")
+                    r = Optional.of(new cuenta(
+                            rs.getString("num_cuenta"),
+                            rs.getInt("tipo_cuenta_id_tipo_cuenta"),
+                            rs.getString("cliente_id_cliente"),
+                            rs.getString("moneda_nombre"),
+                            rs.getDate("fecha_creacion"),
+                            rs.getDouble("limite_transferencia_diaria"),
+                            rs.getInt("activa"),
+                            rs.getDouble("saldo_inicial"),
+                            rs.getDate("fecha_ultima_aplicacion"),
+                            rs.getDouble("saldo_final")
                     ));
                 }
             }
@@ -66,24 +64,29 @@ public class DaoCuenta {
                 | InstantiationException
                 | SQLException ex) {
             System.err.printf("Excepción: '%s'%n", ex.getMessage());
-        }finally {
+        } finally {
             bd.cerrarConexion();
         }
         return r;
     }
 
-    public List<Cliente> obtenerListaClientes() {
-        List<Cliente> r = new ArrayList<>();
+    public List<cuenta> obtenerListaClientes() {
+        List<cuenta> r = new ArrayList<>();
         try (Connection cnx = obtenerConexion();
                 Statement stm = cnx.createStatement();
                 ResultSet rs = stm.executeQuery(CRUD_Cuenta.LISTAR.obtenerComando())) {
             while (rs.next()) {
-                Cliente e = new Cliente(
-                        rs.getString("id_cliente"),
-                        rs.getString("usuario_id_usuario"),
-                        rs.getString("apellidos"),
-                        rs.getString("nombre"),
-                        rs.getString("telefono")
+                cuenta e = new cuenta(
+                        rs.getString("num_cuenta"),
+                        rs.getInt("tipo_cuenta_id_tipo_cuenta"),
+                        rs.getString("cliente_id_cliente"),
+                        rs.getString("moneda_nombre"),
+                        rs.getDate("fecha_creacion"),
+                        rs.getDouble("limite_transferencia_diaria"),
+                        rs.getInt("activa"),
+                        rs.getDouble("saldo_inicial"),
+                        rs.getDate("fecha_ultima_aplicacion"),
+                        rs.getDouble("saldo_final")
                 );
                 r.add(e);
             }
@@ -93,7 +96,7 @@ public class DaoCuenta {
                 | InstantiationException
                 | SQLException ex) {
             System.err.printf("Excepción: '%s'%n", ex.getMessage());
-        }finally {
+        } finally {
             bd.cerrarConexion();
         }
         return r;
@@ -109,8 +112,6 @@ public class DaoCuenta {
         Connection cnx = bd.obtenerConexion();
         return cnx;
     }
-
-    
 
     private DaoCuenta() {
         try {
